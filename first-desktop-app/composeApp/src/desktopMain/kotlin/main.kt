a60e8553ef396ee257861843cc2cd50d285e127d
Caio Vinicius <caioviniciuscv18@gmail.com>
2024-01-05 08:36:58 -0300

    Changing icon app

--- first-desktop-app/composeApp/src/desktopMain/kotlin/main.kt
+++ first-desktop-app/composeApp/src/desktopMain/kotlin/main.kt
@@ -1,10 +1,12 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "first-desktop-app") {
+    val icon = painterResource("icons/cv18-icon.jpg")
+    Window(onCloseRequest = ::exitApplication, title = "Meu primeiro aplicativo Desktop com Kotlin", icon = icon) {
         App()
     }
 }
